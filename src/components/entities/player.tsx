64e0d09077c0a6d799f6900ed769fe7d7c250e6a
Tom Yancey <thyancey84@gmail.com>
2023-07-22 21:20:26 -0400

    restyling

--- src/components/entities/player.tsx
+++ src/components/entities/player.tsx
@@ -6,18 +6,16 @@ import Enemy from './enemy';
 
 const ScCard = styled.div`
   position: relative;
-  padding: 1rem 1rem 2.25rem 1rem;
 
-  filter: drop-shadow(0.5rem 0.7rem 0 var(--color-grey-light)) drop-shadow(0.5rem 0.7rem 0 var(--color-grey-light))
-    drop-shadow(0.25rem 0.25rem 0.5rem var(--color-black));
-  
-  border-radius: 1.5rem;
+  /* filter: drop-shadow(0.5rem 0.7rem 0 var(--color-grey-light)) drop-shadow(0.5rem 0.7rem 0 var(--color-grey-light)) */
+    /* drop-shadow(0.25rem 0.25rem 0.5rem var(--color-black)); */
 
-  background-color: var(--color-white);
+  box-shadow: 0 0 .25rem .25rem var(--color-grey-light);
+  border-radius: 1rem;
 
   display: flex;
   flex-direction: column;
-  gap: 2rem;
+  gap: 0rem;
 
   &.lit-up {
     /* border: 0.75rem solid var(--color-pink); */
@@ -27,21 +25,40 @@ const ScCard = styled.div`
   }
 `;
 
+const ScShadowDiv = styled.div`
+  position:absolute;
+  z-index: -1;
+  inset:0;
+  border-radius: 1rem;
+`
 const ScEnemy = styled.div`
   background-color: var(--color-pink);
-  border-radius: 0.5rem;
-  border-left: 1.1rem solid var(--color-grey-light);
-  border-top: 1.1rem solid var(--color-grey);
+  border-radius: 1rem 1rem 0 0;
+  padding-bottom: 1rem;
+  /* border-left: 1.1rem solid var(--color-grey-light); */
+  /* border-top: 1.1rem solid var(--color-grey); */
+
+  position: relative;
+
+  ${ScShadowDiv}{
+    box-shadow: 0 0 3rem 2rem var(--color-pink);
+  }
 `;
 
 const ScPlayer = styled.div`
   position: relative;
-  
-  background-color: var(--color-blue);
-  border-radius: 0.5rem;
-  border-left: 1.1rem solid var(--color-grey-light);
-  border-top: 1.1rem solid var(--color-grey);
   padding: 1rem;
+  padding-top: 2rem;
+  border-top: 0.25rem solid var(--color-grey-light);
+
+  background-color: var(--color-blue);
+  border-radius: 0 0 1rem 1rem;
+  /* border-left: 1.1rem solid var(--color-grey-light); */
+  /* border-top: 1.1rem solid var(--color-grey); */
+  
+  ${ScShadowDiv}{
+    box-shadow: 0 0 3rem 1rem var(--color-blue);
+  }
 `;
 
 export const Player = () => {
@@ -51,9 +68,11 @@ export const Player = () => {
     <ScCard id='player' className={activeCombos.length > 0 ? 'lit-up' : ''}>
       <ScEnemy>
         <Enemy />
+        <ScShadowDiv />
       </ScEnemy>
       <ScPlayer>
         <SlotMachine />
+        <ScShadowDiv />
       </ScPlayer>
     </ScCard>
   );
